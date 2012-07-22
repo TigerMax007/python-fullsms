bea28120fed0b1f52e47cd00980e840c6b94fbff
Valentin Haenel <valentin.haenel@gmx.de>
2012-07-22 17:43:42 +0200

    make order same as in API

--- fullsms.py
+++ fullsms.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
     --
     u,user= the fullsms.de username
     p,password the fullsms.de password
-    r,receiver= the person to send the message to
     g,gateway= the gateway to use
+    r,receiver= the person to send the message to
     s,sender= the sender to use
     """
     o = options.Options(optspec)
