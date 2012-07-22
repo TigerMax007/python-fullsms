9cbddd7dd11be800a32c30cb99cc734b61ac6771
Valentin Haenel <valentin.haenel@gmx.de>
2012-07-22 17:53:54 +0200

    basic code for sending and checking

--- fullsms.py
+++ fullsms.py
@@ -2,11 +2,53 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import urllib
 import options
 
+BASE_URL = "https://www.fullsms.de/gw/"
+
+def assemble_rest_call(function, parameters):
+    """ Create a URL suitable for making a REST call to fullsms.de
+
+    Parameters
+    ----------
+    function : str
+        the api function to execute
+    parameters : dict
+        the parameters to use in the call
+
+    Returns
+    -------
+    url : str
+        an ready made url
+
+    """
+    query = urllib.urlencode(sorted(parameters.items()))
+    return "%s%s?%s" % (BASE_URL, function, query)
+
+def call(str_):
+    file_like = urllib.urlopen(str_)
+    return file_like.read()
 
 def send(user, password, gateway, receiver, sender, message):
-    pass
+    parameters = {
+            'user': user,
+            'passwort': password,
+            'typ': gateway,
+            'handynr': receiver,
+            'absender': sende,
+            'text': message
+            }
+    rest_str = assemble_rest_call('', parameters)
+    return call(rest_str)
+
+def check(user, password):
+    parameters = {
+            'user': user,
+            'passwort': password,
+            }
+    rest_str = assemble_rest_call('konto.php', parameters)
+    return call(rest_str)
 
 if __name__ == '__main__':
 
