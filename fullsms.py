b85f7ec8408a02a4e961c386759900383f78968c
Valentin Haenel <valentin.haenel@gmx.de>
2012-07-22 18:00:38 +0200

    initial parsing of basic subcommand

--- fullsms.py
+++ fullsms.py
@@ -47,19 +47,23 @@ def check(user, password):
             'user': user,
             'passwort': password,
             }
-    rest_str = assemble_rest_call('konto.php', parameters)
-    return call(rest_str)
+    return assemble_rest_call('konto.php', parameters)
 
 if __name__ == '__main__':
-
+    subcommands = ['send', 'check']
     optspec = """
-    sms [send | check] [opts] <message>
+    sms %s [opts] <message>
     --
     u,user= the fullsms.de username
     p,password the fullsms.de password
     g,gateway= the gateway to use
     r,receiver= the person to send the message to
     s,sender= the sender to use
-    """
+    """ % ('[' + ' | '.join(subcommands) + ']')
     o = options.Options(optspec)
     (opt, flags, extra) = o.parse(sys.argv[1:])
+
+    if extra[0] not in ['check']:
+        options.fatal('invalid subcommand')
+    elif extra[0]:
+        print check('abc', 'def')
