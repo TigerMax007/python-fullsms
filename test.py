6579852085903f2261cc0f63e7c6e56c912a8d2d
Valentin Haenel <valentin.haenel@gmx.de>
2012-08-06 14:09:36 +0200

    simplify copy and paste

--- test.py
+++ test.py
@@ -10,6 +10,14 @@ import nose.tools as nt
 import fullsms as sms
 
 def test_parse_config():
+
+    def make_tempfile(text):
+        """ Utility for creating tempfiles. """
+        config_file = tempfile.NamedTemporaryFile()
+        config_file.writelines(text)
+        config_file.flush()
+        return config_file
+
     # first test that it works as intended
     test_config = textwrap.dedent("""
     [settings]
@@ -19,11 +27,8 @@ def test_parse_config():
     sender   = 0123456789
     receiver = 0123456789
     """)
-    config_file = tempfile.NamedTemporaryFile()
-    config_filename = config_file.name
-    config_file.writelines(test_config)
-    config_file.flush()
-    config = sms.parse_config(config_filename=config_filename)
+    config_file = make_tempfile(test_config)
+    config = sms.parse_config(config_filename=config_file.name)
     expected = {'user': 'MaxMusterman',
                 'password': 'maxmustermangeheim',
                 'gateway': '11',
@@ -31,6 +36,7 @@ def test_parse_config():
                 'receiver': '0123456789'}
     nt.assert_equal(config, expected)
     nt.assert_raises(IOError, sms.parse_config, config_filename="nofile")
+    # check for alternative section name success and failure
     test_config_section = textwrap.dedent("""
     [configuration]
     user     = MaxMusterman
@@ -39,12 +45,9 @@ def test_parse_config():
     sender   = 0123456789
     receiver = 0123456789
     """)
-    config_file = tempfile.NamedTemporaryFile()
-    config_filename = config_file.name
-    config_file.writelines(test_config_section)
-    config_file.flush()
+    config_file = make_tempfile(test_config_section)
     nt.assert_raises(NoSectionError, sms.parse_config,
-            config_filename=config_filename)
+            config_filename=config_file.name)
     config = sms.parse_config(section='configuration',
-            config_filename=config_filename)
+            config_filename=config_file.name)
     nt.assert_equal(config, expected)
