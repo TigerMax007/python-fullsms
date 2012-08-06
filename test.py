ab403badd2ef4f4492eb7b706edf761818623f72
Valentin Haenel <valentin.haenel@gmx.de>
2012-08-06 14:06:10 +0200

    test for alternative section name

--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 
 import textwrap
 import tempfile
+from ConfigParser import NoSectionError
 
 import nose.tools as nt
 
@@ -30,3 +31,20 @@ def test_parse_config():
                 'receiver': '0123456789'}
     nt.assert_equal(config, expected)
     nt.assert_raises(IOError, sms.parse_config, config_filename="nofile")
+    test_config_section = textwrap.dedent("""
+    [configuration]
+    user     = MaxMusterman
+    password = maxmustermangeheim
+    gateway  = 11
+    sender   = 0123456789
+    receiver = 0123456789
+    """)
+    config_file = tempfile.NamedTemporaryFile()
+    config_filename = config_file.name
+    config_file.writelines(test_config_section)
+    config_file.flush()
+    nt.assert_raises(NoSectionError, sms.parse_config,
+            config_filename=config_filename)
+    config = sms.parse_config(section='configuration',
+            config_filename=config_filename)
+    nt.assert_equal(config, expected)
