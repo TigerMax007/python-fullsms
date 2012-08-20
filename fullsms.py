033b8825c0f4456ebc8ff9bd520a44b147f4fb5e
Valentin Haenel <valentin.haenel@gmx.de>
2012-08-20 19:51:24 +0200

    set version to 0.1.0

--- fullsms.py
+++ fullsms.py
@@ -320,7 +320,7 @@ class Options:
 # options.py which was copied verbatim from bup ends here                     #
 ###############################################################################
 
-__version__ = '0.1-dev'
+__version__ = '0.1.0'
 __author__  = 'Valentin Haenel'
 
 BASE_URL = "https://www.fullsms.de/gw/"
